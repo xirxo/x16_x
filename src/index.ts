7da76f2865db376f5b89973e0720db624549079c
xirxo <xirxo.official@hotmail.com>
2021-06-08 05:07:56 +0800

    Removed comments

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,16 @@ const writeByte = new Uint8Array(memory.buffer);
 const cpu = new CPU(memory);
 
 writeByte[0] = instructions.MOV_LIT_R1;
-writeByte[1] = 0x12; // 0x1234
+writeByte[1] = 0x12;
 writeByte[2] = 0x34;
 
 writeByte[3] = instructions.MOV_LIT_R2;
-writeByte[4] = 0xAB; // 0xABCD
+writeByte[4] = 0xAB;
 writeByte[5] = 0xCD;
 
 writeByte[6] = instructions.ADD_REG_REG;
-writeByte[7] = 2; // r1 index
-writeByte[8] = 3; // r2 index
+writeByte[7] = 2;
+writeByte[8] = 3;
 
 cpu.debug();
 
