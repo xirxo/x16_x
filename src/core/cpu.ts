49cd819059151aa1ce57bf1cfd5732214f2bb924
xirxo <xirxo.official@hotmail.com>
2021-06-08 22:46:30 +0800

    Rewrite CPU class in TS for type safety

--- src/core/cpu.ts
+++ src/core/cpu.ts
@@ -18,17 +18,53 @@ export class CPU {
         }, {});
     }
 
+    // A debug method to view data of all of our registers
     public debug(): void {
-        this.registerNames.forEach(name =>
+        return this.registerNames.forEach(name =>
             console.log(`${name}: 0x${this.getRegister(name).toString(16).padStart(4, '0')}`)
         );
     }
 
+    // A view method to view our current memory
+    public view(add: number, n = 8): void {
+        const next = Array.from({ length: n }, (_, i) =>
+            this.mem.getUint8(add + i)
+        ).map(v => `0x${v.toString(16).padStart(2, '0')}`);
+
+        return console.log(`${add.toString(16).padStart(2, '0')}: ${next.join(' ')}`);
+    }
+
+    // Get a specific register's value
     public getRegister(name: string) {
         if (!(name in this.registerMap)) {
             throw new Error(`[x16_x]: [CPU]: No such register: ${name}`);
         }
 
         return this.registers.getUint16(this.registerMap[name]);
+    }
+
+    // Set a specific register to a specific value
+    public setRegister(name: string, value: number): void {
+        if (!(name in this.registerMap)) {
+            throw new Error(`[x16_x]: [CPU]: No such register: ${name}`);
+        }
+
+        return this.registers.setUint16(this.registerMap[name], value);
+    }
+
+    fetch() {
+        const next = this.getRegister('ip');
+        const now = this.mem.getUint8(next);
+
+        this.setRegister('ip', next + 1);
+        return now;
+    }
+
+    fetch16() {
+        const next = this.getRegister('ip');
+        const now = this.mem.getUint16(next);
+
+        this.setRegister('ip', next + 2);
+        return now;
     }
 }
